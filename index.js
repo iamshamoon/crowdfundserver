9569cbfa4e795b459617af081eae60eaecd426d7
Mian Muhammad Faraz <91678575+M-Muhammad-Faraz@users.noreply.github.com>
2023-01-30 14:23:52 +0500

    Added functionality to get and display all challenges

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const fs = require("fs");
+const session = require("express-session");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(cors());
 
 const connection = mysql.createConnection({
@@ -17,6 +27,8 @@ const connection = mysql.createConnection({
 });
 
 const PORT = 8000;
+
+const upload = multer({ dest: "uploads/" });
 
 connection.connect((err) => {
   if (err) {
@@ -29,9 +41,114 @@ connection.connect((err) => {
         if (err) throw err;
       }
     );
+    connection.query(
+      `
+  CREATE TABLE IF NOT EXISTS admin (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    last_signin_time TIMESTAMP
+  )
+`,
+      function (err, results, fields) {
+        if (err) {
+          console.log(err.message);
+        }
+      }
+    );
+  }
+  const createTable = `CREATE TABLE IF NOT EXISTS challenges (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category VARCHAR(255) NOT NULL,
+  region VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  amount INT NOT NULL,
+  description TEXT NOT NULL,
+  image VARCHAR(255),
+  post_time DATETIME DEFAULT CURRENT_TIMESTAMP
+)`;
+
+  connection.query(createTable, function (err, result) {
+    if (err) throw err;
+    console.log("Challenges table created successfully");
+  });
+});
+
+app.post("/admin/login", function (req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  if (email && password) {
+    connection.query(
+      "SELECT * FROM admin WHERE email = ? AND password = ?",
+      [email, password],
+      function (error, results, fields) {
+        if (results.length > 0) {
+          req.session.loggedin = true;
+          req.session.email = email;
+          res.send({ status: 200, message: "Logged in successfully" });
+        } else {
+          res.send({ status: 401, message: "Invalid Credentials" });
+        }
+        res.end();
+      }
+    );
+  } else {
+    res.send({ status: 401, message: "Please enter Email and Password" });
+    res.end();
   }
 });
 
+app.get("/admin/logout", function (req, res) {
+  req.session.destroy();
+  res.send({ status: 200, message: "Logged out successfully" });
+});
+
+app.get("/get-challenges", (req, res) => {
+  connection.query("SELECT * FROM challenges", (err, challenges) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    const challengesList = challenges.map((challenge) => {
+      let image_url = "http://localhost:3000/uploads/" + challenge.image_url;
+      return {
+        id: challenge.id,
+        title: challenge.title,
+        amount: challenge.amount,
+        description: challenge.description,
+        image_url: image_url,
+      };
+    });
+    return res.send(challengesList);
+  });
+});
+
+app.post("/post-challenge", upload.single("image"), (req, res) => {
+  // Get the form data
+  const title = req.body.title;
+  const amount = req.body.amount;
+  const description = req.body.description;
+  const category = req.body.category;
+  const region = req.body.region;
+
+  // Get the image file
+  const image = req.file;
+
+  // Generate a unique file name for the image
+  const imageFileName = `${Date.now()}-${image.originalname}`;
+
+  // Insert the challenge information into the database
+  const query =
+    "INSERT INTO challenges (title, amount, description, category, region, image) VALUES (?, ?, ?, ?, ?, ?)";
+  const values = [title, amount, description, category, region, imageFileName];
+  connection.query(query, values, (error, results) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ message: "Failed to save challenge." });
+    } else {
+      res.status(200).json({ message: "Challenge saved successfully." });
+    }
+  });
+});
 app.post("/register", (req, res) => {
   const { name, email, phoneNo, password, roll } = req.body;
   console.log(roll);
